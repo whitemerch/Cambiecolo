f04d099967c2aeba90406c5ff83c3a413c162162
whitemerch <chakibhamieh@gmail.com>
2022-01-11 02:37:30 +0100

    PPC

--- Server.py
+++ Server.py
@@ -9,12 +9,12 @@ mq = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREAT)
 mqjeu = sysv_ipc.MessageQueue(keyjeu, sysv_ipc.IPC_CREAT)
 playing = True
 bell_lock = threading.Lock()
+offres_lock = threading.Lock()
 
 def deck():
     deck = []
-    l=[]
     transports = ["Shoes", "Bike", "Train", "Car", "Airplane"]
-    for j in transports :
+    for j in transports[:n] :
         for _ in range(5):
             deck.append(j)
     random.shuffle(deck)
@@ -22,68 +22,108 @@ def deck():
 
 def bell(pid):
     bell_lock.acquire()
-    gagner = False
     list = mains[pid]
     if list.count(list[0]) == len(list):
-        gagner = True
+        for i in continuer:
+            continuer[i]=False
+            win = "L'utilisateur"+str(pid)+" a gagné."
+            mq.send(win.encode(), type=i)
     bell_lock.release()
-    return gagner
 
-#sera invoqué chaque fois que quelqu'un update le dictionnaire offres
+#sera invoque chaque fois que quelqu'un update le dictionnaire offres
 def offer_display():
     mes = str(offres).encode()
-    mqjeu.send(mes, type=3)
+    for pid in players:
+        mqjeu.send(mes, type=int(str(pid)+1))
 
-def make_offer():
+def make_offer(pid, list):
+    offres_lock.acquire()
+    offres[pid]=list
+    offres_lock.release()
+    offer_display()
     pass
 
 def accept_offer():
+    offres_lock.acquire()
+    offres_lock.release()
+    offer_display()
     pass
 
+def valide(list, pid):
+    #It transforms both lists into a set to see if the cards given by the player are really in his hand
+    return (set(list)).issubset(set(mains[pid]))
+
 def jouer(pid):
-    mes = "Votre main est: "+str(mains[pid])
-    mes = mes.encode()
-    mq.send(mes, type=pid)
     while playing:
-        mes = "Que voulez vous faire? Faire une offre(F) ou en accepter une offre(A)"
+        if not continuer[pid]:
+            break
+        mes = "Votre main est: " + str(mains[pid])
+        mes = mes.encode()
+        mqjeu.send(mes, type=pid)
+        mes = "Que voulez vous faire? Faire une offre(F), en accepter une(A) ou faire sonner la sonnerie(S) ?"
+        mes = mes.encode()
+        mqjeu.send(mes, type=pid)
+        #The answer of the choice
+        while True:
+            m,_= mqjeu.receive(type=pid)
+            m = m.decode()
+            if m == "F":
+                if not offres[pid]:
+                    msg="Vous ne pouvez pas faire plus d'une offre"
+                    msg = msg.encode()
+                    mqjeu.send(msg, type=pid)
+                else:
+                    msg = "Quelles cartes voulez-vous proposer ?"
+                    msg=msg.encode()
+                    mqjeu.send(msg, type=pid)
+                    echange, _ = mqjeu.receive(type=pid)
+                    echange = echange.decode()
+                    if valide(echange,pid)==True:
+                        make_offer(pid, echange)
+                    else:
+                        msg = "Vous n'avez pas les cartes que vous pretendez avoir"
+                        msg = msg.encode()
+                        mqjeu.send(msg, type=pid)
+            #elif m =="A":
+
+            else:
+                bell(pid)
 
 
-
-
-def connection(n):
-    joueurs = 0
+def connection():
     cartes = deck()
     k = 0
     while True:
         pid, _ = mq.receive(type=1)
         pid = int(pid.decode())
-        if joueurs < n:
-            joueurs += 1
+        if len(players) < n:
+            players.append(pid)
             offres[pid] = []
             mains[pid] = cartes[k:k+5]
             scores[pid] = 0
-            #locks[pid]=threading.Lock()
             dispo[pid] = True
+            continuer[pid] = True
             mes = "Ton identifiant est: " + str(pid)
             mes = mes.encode()
             mq.send(mes, type=2)
             play = threading.Thread(target=jouer, args=(pid,))
             play.start()
-        elif joueurs == n:
-            print("Le jeu peut commencer")
+        elif len(players) >= n:
             mes = "La partie est pleine"
             mes = mes.encode()
             mq.send(mes, type=3)
-            break
         k += 5
 
 
 if __name__ == "__main__":
     with Manager() as manager:
+        players =[]
         offres = manager.dict()
         mains = manager.dict()
         scores = manager.dict()
-        locks = manager.dict()
         dispo = manager.dict()
+        continuer=manager.dict()
         n = int(input("Combien de joueurs dans la partie : "))
-        connection(n)
+        connection = threading.Thread(target=connection)
+        connection.start()
+        connection.join()
--- client.py
+++ client.py
@@ -2,9 +2,16 @@ import sysv_ipc
 import sys
 import os
 import sys
+import tkinter
+import threading
 
 key=300
 keyjeu=400
+
+def offres():
+    while True:
+        m, _ = mq.receive(type=int(str(os.getpid())+1))
+        print(m.decode())
 
 #We connect to the message queue
 try:
@@ -18,16 +25,20 @@ try:
 except sysv_ipc.ExistentialError:
     print("The message queue you're trying to connect to doesn't exist.")
     sys.exit(1)
-
+thread_offres = threading.thread(target=offres)
 #If we want to be connected to the game
 while True:
     n = input("Voulez-vous jouer ? oui/non: ")
-    if n == "oui":
+    if n.lower() == "oui":
         break
+    elif n.lower() == "non":
+        print("Merci au revoir !")
+        sys.exit(1)
     else:
         print("Votre saisie n'est pas valide. Veuillez réessayer.")
 mq.send(str(os.getpid()).encode(), type=1)
 #To listen to the server's answer
+#Just to the connection
 while True:
     m , t = mq.receive()
     if t == 2 :
@@ -36,13 +47,18 @@ while True:
     if t == 3:
         print(m.decode())
         sys.exit(1)
-state = True
+
 while True:
-    {1:True,2:True}
-    #if state:
-    m , t = mq.receive(type=os.getpid())
+    #To receive the choice msg
+    m , t = mqjeu.receive(type=os.getpid())
     m = m.decode()
-        #if m ==
     print(m)
+    while True:
+        msg = int(input("Saisir votre choix: "))
+        if msg.capitalize() != "F" or msg.capitalize() != "A" or msg.capitalize() != "S":
+            print("Votre saisie est incorrecte. Veuileez réessayer")
+        else:
+            break
+
 
 
