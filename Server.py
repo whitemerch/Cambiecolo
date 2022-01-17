94bfe8a0880e956a973bb3de65cdaeebc67d794c
whitemerch <chakibhamieh@gmail.com>
2022-01-17 22:14:56 +0100

    PPC

--- Server.py
+++ Server.py
@@ -9,30 +9,19 @@ import threading
 mains_i = {}
 jeu = []
 
-mains_i = {}
-jeu = []
 
 class MyManager(BaseManager): pass
-MyManager.register('sm')
-m = MyManager(address=("127.0.0.1", 8888), authkey=b'abracadabra')
-m.connect()
-sm=m.sm()
 
-<<<<<<< HEAD
 
 MyManager.register('sm')
 m = MyManager(address=("127.0.0.1", 8888), authkey=b'abracadabra')
 m.connect()
 sm = m.sm()
-=======
-key = 703
->>>>>>> 458945a564918f10f066eed5711a37f30fe356bc
 
 key = 703
 stop=True
 
 def handler(sig, frame):
-<<<<<<< HEAD
     global stop
     if sig == signal.SIGINT:
         stop=True
@@ -50,13 +39,6 @@ def handler(sig, frame):
             os.kill(pid, signal.SIGUSR2)
     print("The game is finished")
     print("This is the points shart")
-=======
-    if sig == signal.SIGUSR2:
-        for pid in mains_i.keys():
-            os.kill(pid, signal.SIGUSR2)
-    print("La partie est terminée")
-    print("voici le tableau des points")
->>>>>>> 458945a564918f10f066eed5711a37f30fe356bc
     print(sm.get_points())
     while True:
         suite = str(input("Do you want to continue ?(yes/no)"))
@@ -65,14 +47,11 @@ def handler(sig, frame):
             run()
             break
         elif suite == "no":
-<<<<<<< HEAD
             for pids in mains_i.keys():
                 try:
                     os.kill(pids, signal.SIGTERM)
                 except OSError:
                     pass
-=======
->>>>>>> 458945a564918f10f066eed5711a37f30fe356bc
             print("Leaving")
             mq.remove()
             sys.exit(1)
@@ -90,10 +69,7 @@ def nomore():
         if stop:
             sys.exit(1)
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 458945a564918f10f066eed5711a37f30fe356bc
 def deck(n):
     global jeu
     jeu = []
@@ -103,7 +79,6 @@ def deck(n):
             jeu.append(j)
     random.shuffle(jeu)
     return jeu
-<<<<<<< HEAD
 
 
 def run():
@@ -112,34 +87,19 @@ def run():
     stop=True
     n = int(input("How many players will play: "))
     print("Waiting for other players...")
-=======
-
-def run():
-    global jeu
-    n = int(input("How many players will play: "))
->>>>>>> 458945a564918f10f066eed5711a37f30fe356bc
     jeu = deck(n)
     global mains_i
     mains_i = {}
     i = 0
     k = 0
     while i < n:
-        print(os.getpid())           #DEBUG
         pid, _ = mq.receive(type=1)
         pid = int(pid.decode())
-<<<<<<< HEAD
         print(pid)  # DEBUG
 
         # Creating our dict of offers and availability which is empty at the beginning
         offres = sm.get_offers()
         offre_i = []
-=======
-        print(pid)                   #DEBUG
-
-        # Creating our dict of offers and availability which is empty at the beginning
-        offres = sm.get_offers()
-        offre_i=[]
->>>>>>> 458945a564918f10f066eed5711a37f30fe356bc
         sm.set_offers(offre_i, pid)
 
         sm.set_flag(True, pid)
@@ -150,7 +110,6 @@ def run():
         msg = msg.encode()
         # Sending the pid of the server to each client so they have it they may need it to ring the bell
         mq.send(msg, type=pid)
-<<<<<<< HEAD
         pid_server = str(os.getpid())
         pid_server = pid_server.encode()
         mq.send(pid_server, type=pid)
@@ -158,41 +117,19 @@ def run():
         k += 5
     stop=False
     thread.start()
-=======
-        pid_server=str(os.getpid())
-        pid_server=pid_server.encode()
-        mq.send(pid_server, type=pid)
-        i += 1
-        k += 5
-
->>>>>>> 458945a564918f10f066eed5711a37f30fe356bc
     for pid, list in mains_i.items():
         main = (' '.join(map(str, list))).encode()
         mq.send(main, type=pid)
 
     signal.signal(signal.SIGUSR2, handler)
-<<<<<<< HEAD
     signal.signal(signal.SIGINT, handler)
     signal.pause()
-=======
-    signal.pause()
-#Fermer les msgs queues
-#Continuer la partie
-#Faire dict de scores
-
->>>>>>> 458945a564918f10f066eed5711a37f30fe356bc
 
 
 if __name__ == "__main__":
     mq = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREAT)
-<<<<<<< HEAD
     points = sm.get_points()
     for pid in mains_i.keys():
         sm.set_points(0, pid)
     thread = threading.Thread(target=nomore)
-=======
-    points=sm.get_points()
-    for pid in mains_i.keys():
-        sm.set_points(0, pid)
->>>>>>> 458945a564918f10f066eed5711a37f30fe356bc
     run()
